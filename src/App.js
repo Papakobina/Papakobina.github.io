1769d4a49056b22ddfbe9e19527c71d73e17fae1
Papakobina <aggreyian3@gmail.com>
2023-01-31 16:46:06 -0400

    added files for Top nav bar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
+import TopNavBar from './components/TopNavBar/TopNav';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
-       
+        <TopNavBar />
       </header>
     </div>
   );
---
+++ src/components/TopNavBar/TopNav.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './TopNav.css'
+
+
+
+function TopNavBarComponent(){
+    return(
+        <div>
+
+        </div>
+    )
+};
+
+export default TopNavBarComponent;
