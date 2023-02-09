c84ee8ab0b6ff722f015418bebe80ceaaeb34033
Papakobina <aggreyian3@gmail.com>
2023-02-09 19:46:54 -0400

    added new component plus clean up code

--- src/components/FirstBodyofHomeComponent/Body.js
+++ src/components/FirstBodyofHomeComponent/Body.js
@@ -11,9 +11,7 @@ function FirstBodyComponent(){
             <div className="headerholder">
                 <div className="Textheader">
                     <h1 className="headertitle">Volta Energy </h1>
-                    {/* <ul className="dynamiclist">
-                        <li><span>Changing the Way You Think About Electricity!</span></li>
-                    </ul> */}
+                   
                 </div>
                 <div className="Subheading">
                 <Typewriter
@@ -30,24 +28,6 @@ function FirstBodyComponent(){
                 <img src={MainImage} alt='images'/>
 
             </div>
-            {/* <div> */}
-            {/* <div className="parallax1"></div>
-                <div className="height:0px">
-                </div>
-
-                <div className="parallax2"></div> */}
-            {/* </div> */}
-        {/* <div className="holder">
-        <button className="button"> 
-            <div className="buttoncontent">
-            Explore More
-            </div>
-        </button>
-            <img src={arrow} alt='arrow' height={45} width={45}/>
-        </div> */}
-            
-
-
         </div>
         </>
     )
