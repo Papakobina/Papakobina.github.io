5b577f323f2dad147cc10baba8105fcf54286e8a
Papakobina <aggreyian3@gmail.com>
2023-02-14 14:30:18 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
 import TopNavBarComponent from './components/TopNavBar/TopNav';
-import FirstBodyComponent from './components/FirstBodyofHomeComponent/Body';
-import SecondBodyComponent from './components/SecondBodyofComponent/body';
-import ThirdBodyComponent from './components/ThirdBodyComponent/body3';
-import FooterComponent from './components/FooterComponent/footerComponent';
-import StatsComponent from './components/StatsComponent/StatsComponent';
-import SpiceComponent from './components/SpiceContainerComponent/SpiceBodyComponent';
+import { Routes, Route } from 'react-router-dom';
+import React from 'react';
+import HomeApp from './HomeApp';
 
 
 function App() {
@@ -14,11 +11,10 @@ function App() {
       <header className="App-header">
         <TopNavBarComponent />
       </header>
-      <FirstBodyComponent />
-      <SecondBodyComponent />
-      <ThirdBodyComponent />
-      <SpiceComponent />
-      <FooterComponent />
+      <Routes>
+        <Route path="/" element={<HomeApp />} />
+      </Routes>
+
 
     </div>
   );
