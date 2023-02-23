b47a4b0e8af453d78c8e2b09287aedd6cca14434
Papakobina <aggreyian3@gmail.com>
2023-02-23 01:57:53 -0400

    built foundation for blog post

--- src/components/BlogPageComponent/BlogPage.js
+++ src/components/BlogPageComponent/BlogPage.js
@@ -1,9 +1,20 @@
 import React from "react";
+import './BlogPage.css'
 
 
 function BlogPageComponent(){
     return(
         <>
+        <div className="blogpagecomponent">
+            <div className="scrollablefeed">
+
+            </div>
+            <div className="verticalLine"></div>
+            <div className="currentblog">
+
+            </div>
+
+        </div>
          
         </>
     );
