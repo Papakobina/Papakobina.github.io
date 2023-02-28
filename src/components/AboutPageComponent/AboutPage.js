6c69dcf13ff637ba1742fc9d02ce683b5f74b54f
Papakobina <aggreyian3@gmail.com>
2023-02-28 17:06:25 -0500

    impolemeneted the features for about us page

--- src/components/AboutPageComponent/AboutPage.js
+++ src/components/AboutPageComponent/AboutPage.js
@@ -9,37 +9,60 @@ import "./AboutPage.css"
 function AboutPageComponent(){
     return(
         <>
-        <div className="aboutuspage">
-            <div className="firstrow">
-                <div className="firstImagecontainer">
-                    <div className="firstimage">
-                        <img src={denis} alt='denis' width={280} height={280}/>
-                    </div>
-                    <div>
-                        <h1 className="firstimageheading">CEO</h1>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-                    </div>
-                </div>
-                <div className="SecondImagecontainer">
-                    <div className="secondimage">
-                        <img src={daniel} alt='daniel' width={280} height={280}/>
-                    </div>
-                </div>
-            </div>
-            <div className="secondrow">
-                <div className="thirdImagecontainer">
-                    <div className="thirdimage">
-                        <img src={john} alt='john' width={280} height={280}/>
-                    </div>
-                </div>
-                <div className="fourthImagecontainer">
-                    <div className="fourthimage">
-                        <img src={adriana} alt='adriana' width={280} height={280}/>
-                    </div>
-                </div>
-
-            </div>
-        </div>
+  <div className="aboutuspage">
+  <div className="firstrow">
+  <div class="firstImagecontainer">
+  <div class="image-container">
+    <img src={denis} alt='denis' width={280} height={280}/>
+    <div class="image-overlay">
+      <div class="image-button">Contact</div>
+    </div>
+  </div>
+  <div class="card">
+    <h1 className="firstimageheading">CEO</h1>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+  </div>
+</div>
+<div class="SecondImagecontainer">
+  <div class="image-container">
+  <img src={daniel} alt='daniel' width={280} height={280}/>
+    <div class="image-overlay">
+      <div class="image-button">Contact</div>
+    </div>
+  </div>
+  <div class="card">
+    <h1 className="firstimageheading">CEO</h1>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+  </div>
+</div>
+  </div>
+  <div className="secondrow">
+  <div class="thirdImagecontainer">
+  <div class="image-container">
+  <img src={john} alt='john' width={280} height={280}/>
+    <div class="image-overlay">
+      <div class="image-button">Contact</div>
+    </div>
+  </div>
+  <div class="card">
+    <h1 className="firstimageheading">CFO</h1>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+  </div>
+</div>
+<div class="fourthImagecontainer">
+  <div class="image-container">
+  <img src={adriana} alt='adriana' width={280} height={280}/>
+    <div class="image-overlay">
+      <div class="image-button">Contact</div>
+    </div>
+  </div>
+  <div class="card">
+    <h1 className="firstimageheading">Marketing</h1>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+  </div>
+</div>
+  </div>
+</div>
         </>
     );
 }
